3ccb747b9e3370af7bbf2ecded0e4e24fc985541
crinq <crinq@crinqbook.local>
2012-08-11 01:56:51 +0200

    Initial Commit

---
+++ cacher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cacher
+//
+//  Created by crinq on 11.08.12.
+//  Copyright (c) 2012 crinq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
